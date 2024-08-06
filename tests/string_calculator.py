cdc79caecd64b8b214f6b2f0b6bb6d3a0d5817f7
samimanvar323 <samim@vlippr.com>
2024-08-06 20:00:45 +0530

    Add test for single number and update add function to handle it

--- tests/string_calculator.py
+++ tests/string_calculator.py
@@ -1,5 +1,11 @@
+#Add test for empty string and initial implementation of add function
+'''def add(numbers: str) -> int:
+    if numbers == "":
+        return 0'''
+
+#Add test for single number and update add function to handle it        
 def add(numbers: str) -> int:
     if numbers == "":
         return 0
-    number_list = numbers.split(",")
-    return sum(int(num) for num in number_list)
+    return int(numbers)   
+
